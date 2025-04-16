3c3d3f1b087b191675f3acf1d45eb7558eb89a45
Anandhakrishnan. S <131889959+AnandhakrishnanS@users.noreply.github.com>
2025-04-16 10:03:44 +0200

    Update company_emp.java

--- company_emp.java
+++ company_emp.java
@@ -1,138 +1,141 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
+abstract class employees {
+    private String first_name;
+    private String last_name;
+    private String ss_num;
+    private String type_con;
+
+    public employees(String first_name, String last_name, String ss_num, String type_con) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.ss_num = ss_num;
+        this.type_con = type_con;
+    }
+
+    public abstract double cal_income();
+
+    public String get_first_name() {
+        return first_name;
+    }
+
+    public String get_last_name() {
+        return last_name;
+    }
+
+    public String get_ss_num() {
+        return ss_num;
+    }
+
+    public String get_type_con() {
+        return type_con;
+    }
+
+    @Override
+    public String toString() {
+        return "first_name: " + first_name + "\nLast_name: " + last_name + "\nSocial_security_number: " + ss_num
+                + "\ncontract_type: " + type_con;
+    }
+}
+
+class full_time_emp extends employees {
+    private double weekly_salary;
+
+    public full_time_emp(String first_name, String last_name, String ss_num, String type_con, int weekly_salary) {
+        super(first_name, last_name, ss_num, type_con);
+        this.weekly_salary = weekly_salary;
+    }
+
+    @Override
+    public double cal_income() {
+        return weekly_salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nposition: Full time" + "\nweekly_salary: " + cal_income();
+    }
+}
+
+class part_time_emp extends employees {
+    private int hours_working;
+    private int hourly_rate;
+
+    public part_time_emp(String first_name, String last_name, String ss_num, String type_con, int hours_working,
+            int hourly_rate) {
+        super(first_name, last_name, ss_num, type_con);
+        this.hours_working = hours_working;
+        this.hourly_rate = hourly_rate;
+    }
+
+    @Override
+    public double cal_income() {
+        return hours_working * hourly_rate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nposition: Part time" + "\nhours_working: " + hours_working + "\nhourly_rate: "
+                + hourly_rate + "\nweekly_salary: " + cal_income();
+    }
+}
+
+class commission_emp extends employees {
+    private int number_of_sales;
+    private double commission_percentage;
+    private double price_of_each_sale;
+
+    public commission_emp(String first_name, String last_name, String ss_num, String type_con, int number_of_sales,
+            double commission_percentage, double price_of_each_sale) {
+        super(first_name, last_name, ss_num, type_con);
+        this.number_of_sales = number_of_sales;
+        this.commission_percentage = commission_percentage;
+        this.price_of_each_sale = price_of_each_sale;
+    }
+
+    @Override
+    public double cal_income() {
+        double tmp = number_of_sales * price_of_each_sale;
+        return (commission_percentage / 100) * tmp;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\npostion: Commission Based" + "\nnumber of sales: " + number_of_sales
+                + "\ncommission percentage: " + commission_percentage + "%" + "\nPrice per sale: " + price_of_each_sale
+                + "\nweekly salary: " + cal_income();
+    }
+}
+
+class base_commission_emp extends commission_emp {
+    private double base_salary;
+
+    public base_commission_emp(String first_name, String last_name, String ss_num, String type_con,
+            double base_salary, int number_of_sales, double commission_percentage, double price_of_each_sale) {
+        super(first_name, last_name, ss_num, type_con, number_of_sales, commission_percentage, price_of_each_sale);
+        this.base_salary = base_salary;
+    }
+
+    @Override
+    public double cal_income() {
+        double tmp = super.cal_income();
+        return tmp + base_salary + (10 / 100.0 * base_salary);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nBase salary: " + base_salary + "\nWeekly salary: " + cal_income();
+    }
+}
+
+
+
 public class company_emp {
-    private Scanner sw = new Scanner(System.in);
-    private ArrayList<employees> employeeList = new ArrayList<>();
+    Scanner sw = new Scanner(System.in);
+    ArrayList<employees> employeeList = new ArrayList<>();
 
-    abstract class employees {
-        private String first_name;
-        private String last_name;
-        private String ss_num;
-        private String type_con;
-
-        public employees(String first_name, String last_name, String ss_num, String type_con) {
-            this.first_name = first_name;
-            this.last_name = last_name;
-            this.ss_num = ss_num;
-            this.type_con = type_con;
-        }
-
-        public abstract double cal_income();
-
-        public String get_first_name() {
-            return first_name;
-        }
-
-        public String get_last_name() {
-            return last_name;
-        }
-
-        public String get_ss_num() {
-            return ss_num;
-        }
-
-        public String get_type_con() {
-            return type_con;
-        }
-
-        @Override
-        public String toString() {
-            return "first_name: " + first_name + "\nLast_name: " + last_name + "\nSocial_security_number: " + ss_num
-                    + "\ncontract_type: " + type_con;
-        }
-    }
-
-    class full_time_emp extends employees {
-        private double weekly_salary;
-
-        public full_time_emp(String first_name, String last_name, String ss_num, String type_con, int weekly_salary) {
-            super(first_name, last_name, ss_num, type_con);
-            this.weekly_salary = weekly_salary;
-        }
-
-        @Override
-        public double cal_income() {
-            return weekly_salary;
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + "\nposition: Full time" + "\nweekly_salary: " + cal_income();
-        }
-    }
-
-    class part_time_emp extends employees {
-        private int hours_working;
-        private int hourly_rate;
-
-        public part_time_emp(String first_name, String last_name, String ss_num, String type_con, int hours_working,
-                int hourly_rate) {
-            super(first_name, last_name, ss_num, type_con);
-            this.hours_working = hours_working;
-            this.hourly_rate = hourly_rate;
-        }
-
-        @Override
-        public double cal_income() {
-            return hours_working * hourly_rate;
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + "\nposition: Part time" + "\nhours_working: " + hours_working + "\nhourly_rate: "
-                    + hourly_rate + "\nweekly_salary: " + cal_income();
-        }
-    }
-
-    class commission_emp extends employees {
-        private int number_of_sales;
-        private double commission_percentage;
-        private double price_of_each_sale;
-
-        public commission_emp(String first_name, String last_name, String ss_num, String type_con, int number_of_sales,
-                double commission_percentage, double price_of_each_sale) {
-            super(first_name, last_name, ss_num, type_con);
-            this.number_of_sales = number_of_sales;
-            this.commission_percentage = commission_percentage;
-            this.price_of_each_sale = price_of_each_sale;
-        }
-
-        @Override
-        public double cal_income() {
-            double tmp = number_of_sales * price_of_each_sale;
-            return (commission_percentage / 100) * tmp;
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + "\npostion: Commission Based" + "\nnumber of sales: " + number_of_sales
-                    + "\ncommission percentage: " + commission_percentage + "%" + "\nPrice per sale: " + price_of_each_sale
-                    + "\nweekly salary: " + cal_income();
-        }
-    }
-
-    class base_commission_emp extends commission_emp {
-        private double base_salary;
-
-        public base_commission_emp(String first_name, String last_name, String ss_num, String type_con,
-                double base_salary, int number_of_sales, double commission_percentage, double price_of_each_sale) {
-            super(first_name, last_name, ss_num, type_con, number_of_sales, commission_percentage, price_of_each_sale);
-            this.base_salary = base_salary;
-        }
-
-        @Override
-        public double cal_income() {
-            double tmp = super.cal_income();
-            return tmp + base_salary + (10 / 100.0 * base_salary);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + "\nBase salary: " + base_salary + "\nWeekly salary: " + cal_income();
-        }
-    }
-
-    public void reg_emp() {
+public void reg_emp() {
         System.out.println("  -----------------------------------------------" + "\n  -----------------------------------------------");
         System.out.println("|       1.Full Time Employee                 |");
         System.out.println("|       2.Part Time Employee                 |");
